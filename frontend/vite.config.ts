ff292b2e84c8499bfb1e619d897fc0e2c3fcdbff
Tom <tom@inmail.me>
2026-04-22 14:21:25 +0200

    fixed npm install issue

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,7 @@
 // vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite"; // Add this line
+import tailwindcss from "@tailwindcss/vite"
 
 // https://vite.dev/config/
 export default defineConfig({
