a2369902d97c5685735f9bfc76b12c31e7c8d755
Tom <tom@inmail.me>
2026-04-22 15:45:27 +0200

    fixed name error

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,44 +1,19 @@
 import { useState } from 'react';
-import { Sparkles, Plus, PieChart, ListChecks } from 'lucide-react';
-import { Header } from './components/header';
+import { Header } from './components/Header';
 import "./index.css";
 
 function App() {
-  const [budget, setBudget] = useState(1000000);
-  const [projects, setProjects] = useState<Project[]>([]); // Same logic as before
+  const [budget, setBudget] = useState(100000)
 
   return (
     <div className="app-container">
-      <Header budget={budget}></Header>
+      <Header budget={budget}> setBudget={setBudget}</Header>
 
       <main className="main-layout">
-        <section className="lg:col-span-10 space-y-6">
-          <div className="flex justify-between items-center mb-2">
-            <h2 className="flex items-center gap-2 text-lg font-bold text-slate-700">
-              <ListChecks className="text-brand-primary" size={20} /> Active Projects
-            </h2>
-            <button className="btn-interactive btn-ghost-style">
-              <Plus size={18} /> New Entry
-            </button>
-          </div>
-
-          <div className="card-project">
-             <input placeholder="Project Name..." className="text-xl font-semibold outline-none w-full" />
-          </div>
-        </section>
-
-        <section className="lg:col-span-2">
-          <div className="panel-summary">
-            <h2 className="flex items-center gap-2 text-lg font-bold mb-8 border-b border-white/10 pb-4">
-              <PieChart className="text-brand-primary" size={20} /> Allocation
-            </h2>
-            
-            {/* COMBINING CLASSES HERE */}
-            <button className="btn-interactive btn-primary-style w-full mt-6">
-              Calculate Results
-            </button>
-          </div>
-        </section>
+        <div className='flex gap-4 col-size-4'>
+            <h1 className="text-bold">Hello World</h1>
+            <h3 className="text-size-xl"> Another one</h3>
+        </div>
       </main>
     </div>
   );
---
+++ frontend/src/components/testcomponent.tsx
@@ -0,0 +1,8 @@
+export function testComponent() {
+    return(
+        <div>
+            <h1 className="text-bold">Hello World</h1>
+            <h3 className="text-size-xl"> Another one</h3>
+        </div>
+    );
+}
